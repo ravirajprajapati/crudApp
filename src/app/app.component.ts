dc676b8ce786db31e8de214ff554651dda50fd4d
ravirajprajapati <rcrajapati14@yahoo.com>
2023-10-13 08:42:28 +0530

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from './Modals/user.modal';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crudApp';
+  userForm!: FormGroup;
+  dataSource!: MatTableDataSource<User>;
+
+  @ViewChild(MatTable) table!: MatTable<User>;
+  displayedColumns: string[] = ['userName', 'lastName', 'email', 'action'];
+  updateData!: User;
+  isTable: boolean = false;
+  isSave: boolean = false;
+
+  tableData: User[] = [
+    {
+      id: 0,
+      firstName: 'abc',
+      lastName: 'abc',
+      email: 'abc@gmail.com',
+    },
+    {
+      id: 1,
+      firstName: 'xyz',
+      lastName: 'xyz',
+      email: 'xyz@gmail.com',
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.userForm = new FormGroup({
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+    this.dataSource = new MatTableDataSource(this.tableData);
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      this.tableData.push(this.userForm.value);
+      this.userForm.reset();
+      this.isTable = false;
+      this.dataSource = new MatTableDataSource(this.tableData);
+    }
+  }
+
+  deleteData(data: User) {
+    this.tableData = this.tableData.slice().filter((item) => {
+      return item.id !== data.id;
+    });
+    this.dataSource = new MatTableDataSource(this.tableData);
+    this.table.renderRows();
+
+    if (this.tableData.length == 0) {
+      this.isTable = true;
+    }
+  }
+
+  editData(data: User) {
+    console.log(data);
+    if (data) {
+      this.updateData = data;
+      this.userForm.get('firstName')?.setValue(data.firstName);
+      this.userForm.get('lastName')?.setValue(data.lastName);
+      this.userForm.get('email')?.setValue(data.email);
+      this.isSave = true;
+    }
+  }
+
+  onUpdate() {
+    console.log(this.userForm.value);
+    this.tableData.forEach((data) => {
+      if (data.id == this.updateData.id) {
+        data.firstName = this.userForm.get('firstName')?.value;
+        data.lastName = this.userForm.get('lastName')?.value;
+        data.email = this.userForm.get('email')?.value;
+      }
+    });
+    this.dataSource = new MatTableDataSource(this.tableData);
+    this.table.renderRows();
+    this.isSave = false;
+    this.userForm.reset();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// module
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +20,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
